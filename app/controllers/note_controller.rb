7da9ba7707794a578914f668becf52e0dcdef388
vitogit <vitogit@users.noreply.github.com>
2016-10-25 10:29:26 -0300

    Clean note_controller

--- app/controllers/note_controller.rb
+++ app/controllers/note_controller.rb
@@ -6,41 +6,10 @@ class NoteController < ApplicationController
     @notes = [Note.create(text:'root')] if @notes.empty?
   end
 
-  # # POST /notes
-  # # POST /notes.json
-  # def create
-  #   @note = Note.new(note_params)
-  #   respond_to do |format|
-  #      if @note.save
-  #         format.html { redirect_to @note, notice: 'Note was successfully created.' }
-  #         format.json { render :show, status: :created, location: @note }
-  #      else
-  #         format.html { render :new }
-  #         format.json { render json: @note.errors, status: :unprocessable_entity }
-  #      end
-  #   end
-  # end
-  # 
-  # # PATCH/PUT /notes/1
-  # # PATCH/PUT /notes/1.json
-  # def update
-  #   respond_to do |format|
-  #      if @note.update(note_params)
-  #         format.html { redirect_to @note, notice: 'Note was successfully updated.' }
-  #         format.json { render :show, status: :ok, location: @note }
-  #      else
-  #         format.html { render :edit }
-  #         format.json { render json: @note.errors, status: :unprocessable_entity }
-  #      end
-  #   end
-  # end
-
-  # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
     @note.destroy
        respond_to do |format|
-       format.html { redirect_to :root, notice: 'Note was successfully destroyed.' }
        format.json { head :no_content }
     end
   end
