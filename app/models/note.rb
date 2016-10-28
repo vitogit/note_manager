f242e00a6ee7f8828a9571c61447de8fd1d5da5e
vito <vitogit@users.noreply.github.com>
2016-10-27 22:34:27 -0300

    Add tags by regex

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,15 +1,23 @@
 class Note < ApplicationRecord
   acts_as_tree
   acts_as_taggable
-  
-  validates :text, presence: true 
-  
+  before_save :parse_add_tags
+  validates :text, presence: true
+
   def toggle_complete
     self.completed = !completed
   end
-  
+
   #tags can be a single string tag, or an array
   def self.search_by_tag (tags)
     Note.tagged_with(tags)
   end
+
+  private
+  def parse_add_tags
+    self.text.scan(/\B#\w+/).each do |tag|
+        self.tag_list.add tag
+    end
+  end
+
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe Note, type: :model do
   it "is valid with valid attributes" do
     expect(Note.new(text: 'hello')).to be_valid
   end
-  
+
   it "is not valid without text" do
     note = Note.new(text: nil)
     expect(note).to_not be_valid
-  end 
-  
+  end
+
   it "is a tree" do
     root      = Note.create(text: "root")
     child1    = root.children.create(text: "child1")
@@ -17,21 +17,21 @@ RSpec.describe Note, type: :model do
     expect(root.children.size).to equal 2
     expect(child1.parent).to equal(root)
   end
-  
+
   it "toggle the complete attribute" do
     note = Note.create(text: "root")
     note.toggle_complete
     expect(note.completed).to equal(true)
     note.toggle_complete
     expect(note.completed).to equal(false)
-  end  
-  
+  end
+
   it "has tags" do
     note = Note.create(text: "root")
     note.tag_list.add("tag1")
     expect(note.tag_list.size).to equal(1)
   end
-  
+
   context "search_by_tag" do
     it "filter notes by a single tag" do
       note1 = Note.create(text: "n1")
@@ -45,7 +45,7 @@ RSpec.describe Note, type: :model do
       expect(Note.search_by_tag("tag3").size).to eq(1)
       expect(Note.search_by_tag("tag4").size).to eq(0)
     end
-    
+
     it "filter notes by an array of tags" do
       note1 = Note.create(text: "n1")
       note1.tag_list.add("tag1", "tag2")
@@ -53,10 +53,21 @@ RSpec.describe Note, type: :model do
       note2.tag_list.add("tag1", "tag3")
       note1.save
       note2.save
-      
+
       expect(Note.search_by_tag(["tag1"]).size).to equal(2)
       expect(Note.search_by_tag(["tag1", "tag2"]).size).to equal(1)
       expect(Note.search_by_tag(["tag3", "tag4"]).size).to equal(0)
     end
+  end
+
+  context "add tags by regex" do
+    it "add tags with alphanumeric and underline chars" do
+      text = " hello #tag_1 #tag_2"
+      note1 = Note.create(text: text)
+
+      expect(Note.search_by_tag("#tag_1").size).to eq(1)
+      expect(Note.search_by_tag("#tag_2").size).to eq(1)
+    end
+
   end
 end
